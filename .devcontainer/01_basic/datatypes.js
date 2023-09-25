08d67fe9d2d599470ebb5a07559524f270a162a5
ayush-maddhesiya <118817485+ayush-maddhesiya@users.noreply.github.com>
2023-09-25 03:14:37 +0000

    variable and datatype

---
+++ .devcontainer/01_basic/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all the code for newer version of js.
+
+//alert(3+3)  // we are using nodejs not bwoswer
+
+console.log(3
+    +3); // this is not good pratice becoz of readbity issue and future modifications
+
+console.log("Ayush maddy");
+
+let name = "ayush"
+let age = 21
+let isLogged = false
+
+// number => 2^53
+// bigint => much larger
+// string => ""
+//boolean => true
+//null => standlone value
+//undefined =>
+//symbol => unique
+
+
+console.log(typeof age); // this tell  type of datatype
---
+++ .devcontainer/01_basic/variables.js
@@ -0,0 +1,22 @@
+const accountId = 145231
+let accountEmail = "Ayushkumarmaddheiay@gamil.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+let accountState
+
+
+//  accountId  =  2  .. this is not possible .
+
+accountEmail = "akumar@gamil.com"
+accountPassword = "98765"
+accountCity = "Deoria"
+
+/*
+plz don't use "var" becoz this create the problem in spoce of variables
+
+*/
+
+console.log(accountId)
+
+//another method to print all at once
+console.table([accountEmail,accountPassword, accountCity,accountState])
