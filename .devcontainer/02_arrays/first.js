c1492ffae11648f8e88036eaeffb59f8b5dab115
Ayush Maddhesiya <118817485+ayush-maddhesiya@users.noreply.github.com>
2023-09-27 03:02:32 +0000

    array frist

---
+++ .devcontainer/02_arrays/first.js
@@ -0,0 +1,33 @@
+
+const array = [1,2,3,4,5];
+
+const heros = ["shaktiman","flyingJatt", "ayush"]
+
+//accessing the array by index;
+console.log(array[2]);
+
+
+//array method
+
+array.push(6);
+array.push(77);
+console.log(array);  
+array.pop();
+console.log(array);
+
+array.unshift(09);
+console.log(array);
+array.shift();
+
+console.log(array);
+
+console.log(array.includes(11));
+//  console.log(array.indexof(4));
+
+const newarr = array.join()
+console.log(newarr);
+
+
+//silce and splice 
+console.log("A : " array);
+
