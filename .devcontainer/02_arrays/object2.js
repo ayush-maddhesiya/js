2944bafea0d6ce1b8acf5a8f26980143f69647db
Ayush Maddhesiya <118817485+ayush-maddhesiya@users.noreply.github.com>
2023-09-28 08:38:08 +0000

    obj two

---
+++ .devcontainer/02_arrays/object2.js
@@ -0,0 +1,42 @@
+/*
+const tinderUser = new Object()  //singleton
+const userTinder = {}      //non-Singleton 
+*/   //both are same but,
+
+const tinderUser = new Object()
+tinderUser.id = "abc123"
+tinderUser.email ="ayush@google.com"
+tinderUser.islogged  = false;
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "kumar@microsoft.com",
+    fullname :{
+        userfullname : {
+            fristname :  "raj",
+            lastname : "rahul",
+            middlename : "akhand"
+
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.middlename);
+
+
+
+//to add object 
+const obj1  ={1:"a",2:"b"}
+const obj2  ={3:"c",4:"d"}
+
+//const obj3  = {obj1,obj2}    //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+
+
+//soo
+const obj3  = Object.assign(obj1,obj2)
+//or 
+
+
+
+console.log(obj3);
