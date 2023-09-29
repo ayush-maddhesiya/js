5e578d1d596142420e647152889fabd70f4ea6e6
Ayush Maddhesiya <118817485+ayush-maddhesiya@users.noreply.github.com>
2023-09-29 06:06:21 +0000

    final iife

--- .devcontainer/03_basics/04_iife.js
+++ .devcontainer/03_basics/04_iife.js
@@ -1,16 +1,3 @@
-// Immediately Invoked Function Expressions (IIFE)
-
-
-(function chai(){
-    // named IIFE
-    console.log(`DB CONNECTED`);
-})();
-
-( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
-
-
 
 //Immeediately invoked Funtions Expressions
 
